424c86ff9ed6988f221127277f8cfeaa9be2839c
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-08-05 11:49:43 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./Sidebar/Sidebar";
+import "./App.css";
+
+function App() {
+  return (
+    
+    <div className="app">
+     <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import TwitterIcon from "@material-ui/icons/Twitter";
+const Sidebar = () => {
+  return (
+    <div>Sidebar
+        <TwitterIcon/>
+    </div>
+  )
+}
+
+export default Sidebar
